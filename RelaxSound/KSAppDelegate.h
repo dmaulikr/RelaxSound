4f594dfd487c9ff4509a9c1516516e3330a4c991
Kevin San 闪奎章 <kuizhang110@foxmail.com>
2014-07-29 17:50:57 +0800

    add a project
    new

---
+++ RelaxSound/KSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KSAppDelegate.h
+//  RelaxSound
+//
+//  Created by kevin on 14-7-29.
+//  Copyright (c) 2014年 com.kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RelaxSound/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RelaxSound
+//
+//  Created by kevin on 14-7-29.
+//  Copyright (c) 2014年 com.kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KSAppDelegate class]));
+    }
+}
